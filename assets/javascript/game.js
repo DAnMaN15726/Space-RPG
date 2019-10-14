aa98ad17fdde6b54175f9f3b2c2084e264d8e81f
danman15726 <danman15726@gmail.com>
2019-10-14 13:45:51 -0500

    Added a successful choice system that tallies each card as it's own seperate choice. Meaning if 4 cards have their animation active, that means 4 on the counter. 3 cards active? 3. So on, so forth. Now the next phase is to set a function in place that prompts the user to ask if they want to continue with the selection once 2 characters have been selected. Dan out.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,90 @@
-$(".btn").click(function(){
-    console.log("1");
-})
+let counter = 0;
+let boolean = false;
+// var toggleChoice = false;
+var toggleChoice1 = false;
+var toggleChoice2 = false;
+var toggleChoice3 = false;
+var toggleChoice4 = false;
+    
+
+$("#Card1").click(function(){
+    // toggleChoice =! toggleChoice;
+    
+    
+    $(this).toggleClass("active", toggleChoice1 =! toggleChoice1); 
+    // console.log(this.id);
+    if( toggleChoice1 === true){
+        counter = counter + 1;
+        console.log(counter);
+    }
+    else if
+    (toggleChoice1 === false){
+        counter = counter - 1;
+        console.log(counter);
+    }
+
+
+
+});
+
+$("#Card2").click(function(){
+    // toggleChoice =! toggleChoice;
+    
+    $(this).toggleClass("active", toggleChoice2 =! toggleChoice2); 
+    // console.log(this.id);
+    if( toggleChoice2 === true){
+        counter = counter + 1;
+        console.log(counter);
+    }
+    else if
+    (toggleChoice2 === false){
+        counter = counter - 1;
+        console.log(counter);
+    }
+
+
+
+});
+
+$("#Card3").click(function(){
+    // toggleChoice =! toggleChoice;
+    
+    
+    $(this).toggleClass("active", toggleChoice3 =! toggleChoice3); 
+    // console.log(this.id);
+    if( toggleChoice3 === true){
+        counter = counter + 1;
+        console.log(counter);
+    }
+    else if
+    (toggleChoice3 === false){
+        counter = counter - 1;
+        console.log(counter);
+    }
+
+
+
+});
+
+$("#Card4").click(function(){
+    // toggleChoice =! toggleChoice;
+   
+    
+    $(this).toggleClass("active", toggleChoice4 =! toggleChoice4); 
+    // console.log(this.id);
+    if( toggleChoice4 === true){
+        counter = counter + 1;
+        console.log(counter);
+    }
+    else if
+    (toggleChoice4 === false){
+        counter = counter - 1;
+        console.log(counter);
+    }
+
+
+
+});
+
+
+
